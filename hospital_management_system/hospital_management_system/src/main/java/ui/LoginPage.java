118e32fd2163580715ebf424d86213b8ef555d49
ayse <ayseekayabay@gmail.com>
2024-05-01 22:02:50 +0300

    first commit

---
+++ hospital_management_system/hospital_management_system/src/main/java/ui/LoginPage.java
@@ -0,0 +1,48 @@
+package ui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class LoginPage {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					LoginPage window = new LoginPage();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public LoginPage() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("DENEME");
+		lblNewLabel.setBounds(182, 62, 45, 13);
+		frame.getContentPane().add(lblNewLabel);
+	}
+}
---
+++ hospital_management_system/hospital_management_system/src/test/java/com/example/hospital_management_system/HospitalManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hospital_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
