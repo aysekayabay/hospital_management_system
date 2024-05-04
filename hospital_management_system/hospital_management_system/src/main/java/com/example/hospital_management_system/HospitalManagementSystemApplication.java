33c4ebb0d91afa77b3cf6713331bdcaf4dc57c83
ayse <ayseekayabay@gmail.com>
2024-05-04 15:09:55 +0300

    ui & spring bug fixed

--- hospital_management_system/hospital_management_system/src/main/java/com/example/hospital_management_system/HospitalManagementSystemApplication.java
+++ hospital_management_system/hospital_management_system/src/main/java/com/example/hospital_management_system/HospitalManagementSystemApplication.java
@@ -6,79 +6,36 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 import com.example.hospital_management_system.entity.*;
 import com.example.hospital_management_system.repository.*;
 
-
+import ui.ViewPatientAndAddRecordPage;
 @SpringBootApplication
 public class HospitalManagementSystemApplication implements CommandLineRunner {
 	
-	private final UserRepository userRepository;
-	private final InsuranceServerRepository insuranceServerRepository;
-	private final PoliclinicRepository policlinicRepository;
-	private final ClinicRepository clinicRepository;
-	private final StaffRepository staffRepository;
-	private final DoctorRepository doctorRepository;
 	private final PatientRepository patientRepository;
-	private final PaymentRepository paymentRepository;
-	private final AppointmentRepository appointmentRepository;
-	private final TreatmentRepository treatmentRepository;
-	private final MedicalProcedureRepository medicalProcedureRepository;
-	private final MedicalProcedureTreatmentRepository medicalProcedureTreatmentRepository;
-	
-	
-	
-	@Autowired
-	public HospitalManagementSystemApplication(UserRepository userRepository, InsuranceServerRepository insuranceServerRepository, PoliclinicRepository policlinicRepository, ClinicRepository clinicRepository, StaffRepository staffRepository, DoctorRepository doctorRepository, PatientRepository patientRepository, PaymentRepository paymentRepository, AppointmentRepository appointmentRepository, TreatmentRepository treatmentRepository, MedicalProcedureRepository medicalProcedureRepository, MedicalProcedureTreatmentRepository medicalProcedureTreatmentRepository) {
-	    this.userRepository = userRepository;
-		this.insuranceServerRepository = insuranceServerRepository;
-		this.policlinicRepository = policlinicRepository;
-		this.clinicRepository = clinicRepository;
-		this.staffRepository = staffRepository;
-		this.doctorRepository = doctorRepository;
-		this.patientRepository = patientRepository;
-		this.paymentRepository = paymentRepository;
-		this.appointmentRepository = appointmentRepository;
-		this.treatmentRepository = treatmentRepository;
-		this.medicalProcedureRepository = medicalProcedureRepository;
-		this.medicalProcedureTreatmentRepository = medicalProcedureTreatmentRepository;
-	}
 
-
-
-    public static void main(String[] args) {
-        SpringApplication.run(HospitalManagementSystemApplication.class, args);
+    public HospitalManagementSystemApplication(PatientRepository patientRepository) {
+        this.patientRepository = patientRepository;
     }
 
-    
-    // This section is for testing the database. Those that I wrote in the comment line has been tested.
-    // Test them one by one to test for yourself.
+    public static void main(String[] args) {
+    	SpringApplicationBuilder builder =new SpringApplicationBuilder(HospitalManagementSystemApplication.class);
+    	builder.headless(false);
+        builder.run(args);
+    }
+   
     @Override
     public void run(String... args) throws Exception {
-        //User user = userRepository.findById(1L).orElse(null);
-    	//InsuranceServer insuranceServer = insuranceServerRepository.findById(1L).orElse(null);
-    	//Policlinic policlinic = policlinicRepository.findById(1L).orElse(null);
-    	//Clinic clinic = clinicRepository.findById(1L).orElse(null);
-    	//Staff staff = staffRepository.findById(1L).orElse(null);
-    	//Doctor doctor = doctorRepository.findById(1L).orElse(null);
-    	//Patient patient = patientRepository.findBySocialNumber("12345678901"); // Since the Patient ID is String, we defined a special method
-    	//Payment payment = paymentRepository.findById(1L).orElse(null);
-    	//Appointment appointment = appointmentRepository.findById(1L).orElse(null);
-    	//Treatment treatment = treatmentRepository.findById(1L).orElse(null);
-    	MedicalProcedure medicalProcedure = medicalProcedureRepository.findById(1L).orElse(null);
- 
-
-        
-       if (medicalProcedure != null) {
-            System.out.println(medicalProcedure);
-            System.out.println(medicalProcedure.getName());
-        } else {
-            System.out.println("Kullanıcı bulunamadı.");
+        // Id'si 1 olan hastayı al
+        Patient patient = patientRepository.findBySocialNumber("12345678901");
+        System.out.println("!!!!!!!!! " + patient.getFirstName());
+        if (patient != null) {
+            // UI'ı başlat ve hastayı gönder
+            ViewPatientAndAddRecordPage window = new ViewPatientAndAddRecordPage(patient);
+            window.getFrame().setVisible(true);
         } 
-       
-       // MedicalProcedureTreatment iki tane anahtara sahip olduğundan testi aşağıdaki gibi olmalı
-   	    Optional<MedicalProcedureTreatment> result = medicalProcedureTreatmentRepository.findByMedicalProcedureIdAndTreatmentId(1L, 1L);
-   	    result.ifPresent(System.out::println);
     }
 }
